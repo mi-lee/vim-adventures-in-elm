b5b030c9762aa4519ed2063a773ffe7d893f1db6
Michelle Lee <michelle.mh.lee92@gmail.com>
2018-11-18 13:13:52 -0800

    Added key bindings -- now appends key input to model text

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,59 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 import Browser
 import Html exposing (Html, Attribute, div, span, text)
 import Html.Attributes exposing (..)
+import Browser
+import Browser.Events as Events
 import Html.Events exposing (onInput)
-import String
--- import Debug
+import Json.Decode as Decode
 
+import String
+
+main : Program () Model Msg
 main =
-    Browser.sandbox {init = init, update = update, view = view }
+    Browser.element
+        { view = view
+        , init = \() -> init
+        , subscriptions = subscriptions
+        , update = update
+        }
 
 type alias Model =
     { world : String,
       point: Int
     }
 
-init : Model
+init : ( Model, Cmd Msg )
 init =
-    { world = "Hello World\nNewline?\nExcellent",
-      point = 7}
+    ({ world = "Hello World\nNewline?\nExcellent",
+          point = 7},
+          Cmd.none
+          )
 
 type Msg
-    = Char
+    = KeyDowns String
+    | ClearPressed
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model
+    case msg of
+      KeyDowns code ->
+        ( {model | world = (model.world ++ code)}, Cmd.none )
+      ClearPressed ->
+            ( model, Cmd.none )
 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Events.onKeyDown (Decode.map KeyDowns keyDecoder)
+        , Events.onKeyUp (Decode.succeed ClearPressed)
+        ]
+
+
+keyDecoder : Decode.Decoder String
+keyDecoder =
+    Decode.field "key" Decode.string
 
 
 view : Model -> Html Msg
