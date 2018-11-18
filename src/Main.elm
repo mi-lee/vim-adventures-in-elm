490232f71b2bcad24e6d017dfcfeec63e9ece032
Michelle Lee <michelle.mh.lee92@gmail.com>
2018-11-18 13:29:28 -0800

    Formatted with elm-format.

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,13 @@
 module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
-import Browser
-import Html exposing (Html, Attribute, div, span, text)
-import Html.Attributes exposing (..)
+
 import Browser
 import Browser.Events as Events
+import Html exposing (Attribute, Html, div, span, text)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Json.Decode as Decode
-
 import String
+
 
 main : Program () Model Msg
 main =
@@ -18,28 +18,34 @@ main =
         , update = update
         }
 
+
 type alias Model =
-    { world : String,
-      point: Int
+    { world : String
+    , point : Int
     }
+
 
 init : ( Model, Cmd Msg )
 init =
-    ({ world = "Hello World\nNewline?\nExcellent",
-          point = 7},
-          Cmd.none
-          )
+    ( { world = "Hello World\nNewline?\nExcellent"
+      , point = 7
+      }
+    , Cmd.none
+    )
+
 
 type Msg
     = KeyDowns String
     | ClearPressed
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-      KeyDowns code ->
-        ( {model | world = (model.world ++ code)}, Cmd.none )
-      ClearPressed ->
+        KeyDowns code ->
+            ( { model | world = model.world ++ code }, Cmd.none )
+
+        ClearPressed ->
             ( model, Cmd.none )
 
 
@@ -58,9 +64,12 @@ keyDecoder =
 
 view : Model -> Html Msg
 view model =
-    div [ style "white-space" "pre",
-          style "font-family" "monospace"]
-        [ text (String.slice 0 model.point model.world),
-          span [ style "background-color" "fuchsia" ]
-               [ text (String.slice model.point (model.point + 1) model.world)],
-          text (String.dropLeft (model.point + 1) model.world)]
+    div
+        [ style "white-space" "pre"
+        , style "font-family" "monospace"
+        ]
+        [ text (String.slice 0 model.point model.world)
+        , span [ style "background-color" "fuchsia" ]
+            [ text (String.slice model.point (model.point + 1) model.world) ]
+        , text (String.dropLeft (model.point + 1) model.world)
+        ]
