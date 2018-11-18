bf766fb497494aec64eeadd6f69ca1707fe82f07
Peter <pchodgson11@gmail.com>
2018-11-18 14:19:28 -0800

    started implementing operators

--- src/Main.elm
+++ src/Main.elm
@@ -24,6 +24,36 @@ type alias Model =
     , point : Int
     }
 
+-- Operator functions
+-- Moves point forward/backward by a character
+forward: Model -> Model
+forward model =
+    { model | point = model.point + 1} -- TODO test for EoL, EoF
+backward model =
+    { model | point = model.point - 1} -- TODO test for EoL, BoF
+
+-- Moves point up/down lines
+upward model =
+    model -- TODO traverse back to EoL to measure column(get-column), traverse back to next EoL and forward to appropriate column,
+downward model =
+    model -- TODO " forward
+
+-- Moves point to beginning/end of lines
+startline model =
+    model -- TODO traverse back to EoL
+endline model =
+    model -- TODO traverse forward to EoL
+
+-- Utility functions
+getcolumn: Model -> Int -- Gets column of point
+getcolumn model =
+    1 -- TODO traverse back to EoL to measure
+
+-- Traverses string
+--  for number of characters
+--  until it encounters a character that returns True
+-- Reports the distance travelled? Or new Model?
+-- traverse: Model -> Int -> (Char -> Bool) -> Model
 
 init : ( Model, Cmd Msg )
 init =
@@ -43,8 +73,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         KeyDowns code ->
-            ( { model | world = model.world ++ code }, Cmd.none )
-
+            case code of
+                "h" -> (backward model, Cmd.none)
+                "l" -> (forward model, Cmd.none)
+                _   -> ( { model | world = model.world ++ code }, Cmd.none )
         ClearPressed ->
             ( model, Cmd.none )
 
